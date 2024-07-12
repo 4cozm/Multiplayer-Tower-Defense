f42e9ace8079ff63a0c945809811adda4c3b09a0
4cozm <17328rm@gmail.com>
2024-07-12 20:42:40 +0900

    ♻️ refactor config 파일 가독성 부분 수정
    DB관련 부분을 하나로 묶음

--- server/util/config.js
+++ server/util/config.js
@@ -7,10 +7,13 @@ const configs = {
   serverPort: process.env.PORT,
   serverHost: process.env.HOST,
   jwtSecret: process.env.JWT_SECRET,
-  dbHost: process.env.DB_HOST,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbPort: process.env.DB_PORT,
+  databases: {
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbPort: process.env.DB_PORT,
+    dbName:process.env.DB_NAME,
+  },
 };
 
 export default configs;
