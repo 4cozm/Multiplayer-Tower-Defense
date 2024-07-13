fc0afedf0729e0bdeaf642a2244e4e5a4d6bc653
4cozm <17328rm@gmail.com>
2024-07-13 21:53:00 +0900

    🩹 fix: webSocket 관련 CORS 문제 해결

--- server/init/socket.js
+++ server/init/socket.js
@@ -1,11 +1,18 @@
-// import { Server as SocketID } from 'socket.io';
-// import registerHandler from '../handler/register.handler.js';
+import { Server as SocketIO } from 'socket.io';
+import registerHandler from '../handler/register.handler.js';
 
-// const initSocket = (server) => {
-//   const io = new SocketID();
-//   io.attach(server);
+const initSocket = (server) => {
+  const io = new SocketIO(server, {
+    cors: {
+      origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // 도메인 허용
+      methods: ['GET', 'POST'], // 허용할 HTTP 메서드
+      allowedHeaders: ['Content-Type'], // 허용할 HTTP 헤더
+      credentials: true, // 자격 증명 허용
+    },
+  });
+  io.attach(server);
+  // 클라이언트로부터 오는 이벤트를 처리할 핸들러를 서버에 등록
+  registerHandler(io);
+};
 
-//   registerHandler(io);
-// };
-
-// export default initSocket;
+export default initSocket;
