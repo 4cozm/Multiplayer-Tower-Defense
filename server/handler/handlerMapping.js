6b7281009a24cd6ba165fbd14f5b0a01d77fa5b9
rladmswlr <alswndmswlr@naver.com>
2024-07-15 02:57:18 +0900

    ✨ update 게임오버기능 임시구현(대기)

--- server/handler/handlerMapping.js
+++ server/handler/handlerMapping.js
@@ -2,6 +2,7 @@ import { matchGame } from './match.handler.js';
 import connectionTest from './test.handler.js';
 import initialData from './init.handler.js';
 import { spawnMonster } from './monster.handler.js';
+import { endGame } from './game.handler.js';
 
 const handlerMappings = {
   1: matchGame, //현재는 안쓰는중
@@ -15,7 +16,7 @@ const handlerMappings = {
   //   13: damageMonster,
   //   14: monsterAttackBase,
   //   15: checkForBreak,
-  //   20: gameEnd,
+  20: endGame,
   40: spawnMonster,
   999: connectionTest,
 };
